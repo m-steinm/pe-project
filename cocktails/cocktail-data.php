3b2e866838188c483cfe766e4bdaa1872f6ee11e
Max <steinmetz.max@gmail.com>
2023-08-04 22:42:18 -0400

    update cocktail data

--- cocktails/cocktail-data.php
+++ cocktails/cocktail-data.php
@@ -4,70 +4,85 @@ $cocktailData = [
 	
 	[
 		"id" => 0,
+		"house" => false,
 		"name" => "Aphrodite",
 		"base" => ".75 oz Mezcal",
-		"ingredient-1" => ".75 oz Capaletti",
-		"ingredient-2" => ".75 oz Roots Ditkamo",
-		"ingredient-3" => " .75 oz Lime Juice",
+		"ingredients" => [
+			".75 oz Capaletti", 
+			".75 oz Roots Ditkamo", 
+			".75 oz Lime Juice", 
+		],
 		"garnish" => "Lime twist",
 		"notes" => "Combine ingredients in a shaker and shake until chilled. Serve up in a coupe. Garnish with a twist of lime. "
 
  	],
 	[
 		"id" => 1,
+		"house" => true,
 		"name" => "Nonna's Garden",
 		"base" => "1 oz Beet Gin Blend",
-		"ingredient-1" => ".5 oz Italicus",
-		"ingredient-2" => ".5 oz Lemon Juice",
-		"ingredient-3" => " Top with bubbles",
+		"ingredients" => [
+			".5 oz Italicus", 
+			".5 oz Lemon Juice", 
+			"Top with bubbles", 
+		],
 		"garnish" => "Carrot spritz",
 		"notes" => "Add Beet Gin Blend, Italicus, simple, and lemon to shaker; shake until chilled, serve up in coupe, top bubbly, finish with carrot  spritz."
 
  	],
  	[
 		"id" => 2,
+		"house" => true,
 		"name" => "Verão em Chios",
 		"base" => "1.5 oz Cachaça",
-		"ingredient-1" => ".5 oz Mastiha",
-		"ingredient-2" => ".5 oz Simple",
-		"ingredient-3" => "4 Lime Wedges",
-		"ingredient-4" => "3 Mint Leaves",
+		"ingredients" => [
+				".5 oz Mastiha",
+				".5 oz Simple",
+				"4 Lime Wedges",
+				"3 Mint Leaves",
+		],
 		"garnish" => "",
 		"notes" => "Add all ingredients to shaker, squeeze limes as you’re putting them in. Shake, dirty dump rocks glass. No additional garnish.",
 
  	],
  	[
 		"id" => 3,
+		"house" => true,
 		"name" => "Nardini Nella Grande Mela",
 		"base" => "",
-		"ingredient-1" => "1.5 oz Bourbon",
-		"ingredient-2" => "1.5 oz Amaro Nardini",
-		"ingredient-3" => "Orange Blossom Rinse",
-		"ingredient-4" => "1 dash Angostura Bitters",
-		"ingredient-5" => "1 dash Black Walnut Bitters",
+		"ingredients" => [
+			"1.5 oz Bourbon",
+			"1.5 oz Amaro Nardini",
+			"Orange Blossom Rinse",
+			"1 dash Angostura Bitters",
+			"1 dash Black Walnut Bitters",
+				],
 		"garnish" => "ribboned orange peel and cherry on cocktail stick",
 		"notes" => "Add Bourbon, Nardini, Ango and Black Walnut bitters to mixing glass. Take mini spray bottle (on service well rail) labeled OJ and spray once into rocks glass. Place big rock into glass, stir mix in mixing glass. Strain into rinsed rocks glass. Garnish with ribboned orange peel and cherry on cocktail stick."
 
  	],
  	[
 		"id" => 4,
+		"house" => true,
 		"name" => "Nikolaki",
 		"base" => "1.5 oz Vodka",
-		"ingredient-1" => ".5 oz Lemon Juice",
-		"ingredient-2" => "1 oz Honey Syrup",
-		"ingredient-3" => "Barspoon Greek yogurt",
-		"ingredient-4" => "Half sprig rosemary",
+		"ingredients" => [
+			".5 oz Lemon Juice",
+			"1 oz Honey Syrup",
+			"Barspoon Greek yogurt",
+			"Half sprig rosemary",
+		],
 		"garnish" => "Sprig of Rosemary",
 		"notes" => "Add all ingredients to shaker; shake, serve up in coupe, garnish with mini clothespin/rosemary."
 
  	],
  		[
 		"id" => 5,
+		"house" => true,
 		"name" => "",
 		"base" => "",
-		"ingredient-1" => "",
-		"ingredient-2" => "",
-		"ingredient-3" => " ",
+		"ingredients" => [
+		],
 		"garnish" => "",
 		"notes" => ""
 
--- cocktails/house-list.php
+++ cocktails/house-list.php
@@ -1,19 +1,32 @@
-<?php include('cocktail-data.php'); ?>
+<?php include('cocktail-data.php');
+	foreach ($cocktailData as $item) {
+		if($item["house"] == true){
+			// ?
+		
+		}else{
+				echo "classic cocktail";
+		}
+	};
+
+
+
+ ?>
 
 <section>
 	<inner-column>
 		<h1>House Cocktails</h1>
-		<ul>
+		<ul class="cocktail-grid">
 			<?php foreach ($cocktailData as $cocktail) { ?>
 			<li class="cocktail">
 				<cocktail-card>
 					<h2 class="name"> <a href="?page=detail&cocktail=<?=$cocktail["id"]?>"><?=$cocktail["name"]?></a></h2>
 					<ul>
 						<li class="base"><?=$cocktail["base"]?></li>
-						<li class="ingredient-1"><?=$cocktail["ingredient-1"]?></li>
-						<li class="ingredient-2"><?=$cocktail["ingredient-2"]?></li>
-						<li class="ingredient-3"><?=$cocktail["ingredient-3"]?></li>
-						<li class="garnish"><?=$cocktail["garnish"]?></li>
+						<?php foreach ($cocktail["ingredients"] as $ingredient) { ?>
+							<li><?=$ingredient?></li>
+
+						<?php } ?>
+						<li class="garnish"><span class="garnish-color">Garnish: </span><?=$cocktail["garnish"]?></li>
 					</ul>
 			
 				</cocktail-card>
