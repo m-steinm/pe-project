5b7cef6c24ce743d87b81781fc5070dd21dac9b7
Max <steinmetz.max@gmail.com>
2023-08-08 11:28:06 -0400

    1. started to add css to theme template
    2. moved files inside the repository

---
+++ head.php
@@ -0,0 +1,21 @@
+<?php include('config.php'); ?>
+
+<!doctype html>
+
+<html lang="en">
+	
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Max Steinmetz</title>
+		<meta name="description" content="[[description goes here]]">
+		<meta property="og:image" content="[[meta image source">
+		<base href="<?BASE_URL?>">
+
+		<link rel="stylesheet" href="css/style.css">
+
+<!-- font -->
+		<link href="https://fonts.googleapis.com/css2?family=Eczar&display=swap" rel="stylesheet">
+
+	</head>
---
+++ projects/cocktails/classic-list.php
@@ -0,0 +1,6 @@
+
+<section>
+	<inner-column>
+		<h1>Classic Cocktails</h1>
+	</inner-column>
+</section>
