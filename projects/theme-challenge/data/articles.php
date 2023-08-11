13306ecd208c53d13095861a7e93b4fc26cffb05
Max <steinmetz.max@gmail.com>
2023-08-10 20:43:06 -0400

    fixed hero image on theme challenge

--- projects/theme-challenge/data/articles.php
+++ projects/theme-challenge/data/articles.php
@@ -4,31 +4,31 @@ $articles = [
 	[
 		"heading" => "Article or product",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "coffee-bag-circle.png",
 	],
 	[
 		"heading" => "Destination or option",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "coffee-bag-circle.png",
 	],
 	[
 		"heading" => "Story or something",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "coffee-bag-circle.png",
 	],
 	[
 		"heading" => "Banana tandori",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "coffee-bag-circle.png",
 	],
 	[
 		"heading" => "Bar B Q",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "coffee-bag-circle.png",
 	],
 	[
 		"heading" => "Marsala",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "coffee-bag-circle.png",
 	],
 ];
