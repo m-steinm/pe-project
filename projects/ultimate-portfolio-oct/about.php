48b80c906ce59cb5085921b3ba228cd2b9e2d6bd
Max <steinmetz.max@gmail.com>
2023-11-01 11:46:30 -0400

    Add inital php portfolio project commit

---
+++ projects/ultimate-portfolio-oct/about.php
@@ -0,0 +1,7 @@
+<?php include('header.php'); ?>
+
+<h1>About</h1>
+
+<p>Lorem, ipsum <a href="contact.php">Contact</a> dolor sit amet, consectetur adipisicing elit. Animi praesentium, quo dignissimos porro dicta sint dolore earum accusantium debitis commodi provident expedita inventore perferendis minima doloremque voluptate! Vitae, aut, minus?</p>
+
+<?php include('footer.php'); ?>
